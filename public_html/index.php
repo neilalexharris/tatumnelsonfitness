397bf81aa8acf9f8d619ad5c92b985b82b54cf6c
Neil Harris <neilalexharris@gmail.com>
2016-10-26 07:55:53 +0100

    Remove guff from DOCTYPE and HTML header tags

--- public_html/index.php
+++ public_html/index.php
@@ -3,8 +3,8 @@
 	$logoPath = "Images/Pngs/main_green.png";
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
 		
 		<?php include "header.html"; ?>
