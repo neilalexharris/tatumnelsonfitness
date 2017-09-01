14d44a44298ad88916a106cd106bbe4b1fcf8164
Neil Harris <neilalexharris@gmail.com>
2017-09-01 08:54:07 +0100

    Replace .html with .php; add includes to index.php

---
+++ public_html/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+	<head>
+
+		<?php include 'head.html'; ?>
+
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.js"></script>
+
+		<script>
+			$(document).ready(
+				function (){
+				$('.highlight').addClass("green");
+			});
+		</script>
+
+		<script type="text/javascript">
+
+		 	 var _gaq = _gaq || [];
+			  _gaq.push(['_setAccount', 'UA-23430411-1']);
+			  _gaq.push(['_trackPageview']);
+
+			  (function() {
+			    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			  })();
+
+		</script>
+
+	</head>
+
+	<body>
+
+		<div id='header'>
+			<?php include 'header.html'; ?>
+		</div>
+
+		<!-- <div id="footer_banner">
+			<h3>Personal Training &amp; Pilates in Sevenoaks, Tonbridge &amp; Tunbridge Wells</h3>
+		</div> -->
+
+		<div id='menu'>
+			<?php include 'menu.html'; ?>
+		</div>
+
+		<div class="back-image">
+			<img class="banner" src="Images/Photos/tatum_banner_textp.png" alt="Tatum Nelson Fitness main banner"/>
+		</div>
+
+		<div id='main'>
+			<?php include 'main.html'; ?>
+		</div>
+
+		<div id='social'>
+			<?php include 'social.html'; ?>
+		</div>
+
+		<div id='footer'>
+			<?php include 'footer.html'; ?>
+		</div>
+
+	</body>
+
+</html>
