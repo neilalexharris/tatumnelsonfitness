8012fc05d9786e814e8b9357af8bdd9d8e710194
Neil Harris <neilalexharris@gmail.com>
2020-02-08 14:03:03 +0000

    Add pilates page, update menu

--- public_html/pilates.php
+++ public_html/pilates.php
@@ -1,64 +1,65 @@
-<?php
-	$colClass = "greentext";
-	$logoPath = "Images/Pngs/main_green.png";
-?>
-
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 	<head>
-		
-		<?php include "header.html"; ?>
+
+		<?php include 'head.html'; ?>
 
 		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
 		<script type="text/javascript" src="js/jquery.js"></script>
-		<script type="text/javascript" src="js/jquery.innerfade.js"></script>
 
-		<script type="text/javascript">
-		$(document).ready(
-				function(){
-		
-					$('#banner').innerfade({
-						speed: 2000,
-						timeout: 7000,
-						type: 'sequence',
-						containerheight: '320px'
-					});
-			
+		<script>
+			$(document).ready(
+				function (){
+				$('.highlight').addClass("pink");
+				$('#found_header').click(
+					function(){
+						$('#found_text').slideToggle(1000)
+						});
+				$('#work_header').click(
+					function(){
+						$('#work_text').slideToggle(1000)
+						});
+				$(".dynamic").hide();
 			});
 		</script>
 
 		<script type="text/javascript">
 
-		  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-23430411-1']);
-		  _gaq.push(['_trackPageview']);
+			 var _gaq = _gaq || [];
+				_gaq.push(['_setAccount', 'UA-23430411-1']);
+				_gaq.push(['_trackPageview']);
 
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+				(function() {
+					var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+					ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+				})();
 
 		</script>
+
 	</head>
 
 	<body>
-		
-		<?php include 'menu.php'; ?>
 
-		<table class="main" cellpadding="0", cellspacing="10">
-			<tr>
-				<td class ="greyborder">
-					<p class="<?php echo $colClass; ?>"><b>Info coming soon!</b></p>
-					</br>
-			
-					<?php include 'social.html'; ?>
-					
-				</td>
-			</td>
-		</table>
+		<div id='header'>
+			<?php include 'header.html'; ?>
+		</div>
 
-		<?php include 'footer.php'; ?>
+		<div id='menu'>
+			<?php include 'menu.html'; ?>
+		</div>
+
+		<div id="main">
+			<h1 class='highlight'>Info coming soon!</h1>
+		</div>
+
+		<div id='social'>
+			<?php include 'social.html'; ?>
+		</div>
+
+		<div id='footer'>
+			<?php include 'footer.html'; ?>
+		</div>
 
 	</body>
 </html>
