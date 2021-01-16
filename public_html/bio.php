fc7cd33006720e6b954aa4debc55b5f65c21e432
Neil Harris <neilalexharris@gmail.com>
2021-01-16 16:08:39 +0000

    Update bio page

--- public_html/bio.php
+++ public_html/bio.php
@@ -40,30 +40,50 @@
 			<?php include 'menu.html'; ?>
 		</div>
 
+
 		<div id="main" class="lightblue">
-			<h1 class="lightblue">About Me</h1>
-			<br/>
+			<div class='row'>
+				<div class='col-12'>
+					<h1 class="lightblue pad-block">About Me</h1>
+				</div>
+			</div>
+			<div class='row'>
+				<div class='col-7 pad-block'>
+					<br/>
+					<p>
+						My background is as an international track and field athlete, with a wealth of experience over 100m, 200m and 400m.
+						I competed for Great Britain and England on many occasions, working with and training alongside some of the World’s
+						best coaches and athletes. I incorporate the benefits of this rare insight into the world of elite sport with all
+						my personal training. I want to share the knowledge I have gained, as all of this experience can be adapted and
+						applied to benefit PT sessions.
+					</p></br>
 
-			<p>Having a wealth of experience as an elite athlete over 100m, 200m and 400m, Tatum competed for Great Britain and
-			England on many occasions and she offers a rare insight into the world of elite sport</p>
-			<br/>
+					<p>
+						Throughout my career I was hampered by injuries and illness, this taught me great resilience and how to adapt
+						my training to keep progressing. It was a huge lesson in the right and wrong ways of exercising the body! This
+						has given me a natural intuition in prescribing the right exercises for my clients, ensuring they can have a
+						long and healthy relationship with exercise, doing types of exercise that are sustainable and become part of
+						their lifestyle.
+					</p></br>
 
-			<p>Tatum started her career at the age of 14 as one of the best juniors in the World over the sprint
-			distances. Her accolades include World Schools 100m Silver medallist and 4x100m Gold medallist; European Junior 100m
-			finalist and 4x100m Silver medallist, and still features in Britain's top 10 fastest ever times for 100m and 200m.</p>
-			<br/>
+					<p>
+						I also trained as a dancer, staying with dance up to Elementary level in ballet, until my athletics career really
+						took off and I had to specialise. This gave me an entirely different perspective of training and conditioning,
+						and is something I fuse into my training.
+					 </p></br>
 
-			<p>She has trained with many of athletics' elite, and has also worked with sports people from other disciplines including
-			Olympic swimmer Mark Foster and former All Black rugby international Jonah Lomu</p>
-			<br/>
-
-			<p>During her sporting career Tatum travelled to Europe, the Middle East, America and Australia, gaining experience in how
-			other nations approach fitness and she incorporates this into her training programmes</p>
-			<br/>
-
-			<p>Also a trained dancer, Tatum completed ballet training to Elementary Level, and it was here that her
-			passion for fitness began</p>
-			</br>
+					 <p>
+						 As well as now being a PT, I am also a Mum to 2 lovely children (and a dog!), which gives me a great understanding
+						 of the time constraints and pressures of family life. My training is always tailored to be sympathetic to the
+						 external stresses in life - whatever they may be - with a strong focus on overall wellbeing.
+					 </p>
+				</div>
+				<div class='col-1'>
+				</div>
+				<div class='col-4 pad-block'>
+					<img id="bio" src="images/Photos/tatum_bio2.jpg" width="300px" alt="tatum_nelson_female_personal_trainer">
+		 		</div>
+		 	</div>
 		</div>
 
 		<div id='social'>
